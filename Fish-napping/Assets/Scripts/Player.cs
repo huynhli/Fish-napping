37816c34181ba7f3589c6d37a459515e55147e34
TrachScor6 <liam.huynh@mail.utoronto.ca>
2025-05-01 01:18:14 -0400

    Animations work
    All animations should work.
    
    - switch to unity input package
    - background moving

--- Fish-napping/Assets/Scripts/Player.cs
+++ Fish-napping/Assets/Scripts/Player.cs
@@ -4,43 +4,61 @@ public class Player : MonoBehaviour
 {
     private SpriteRenderer spriteRenderer;
     private Rigidbody2D playerRigidBody;
-    private int spriteIndex;
-    public Sprite[] idleSprites;
-
+    private float healthPoints;
+    public bool isGrounded;
+    private bool facingRight;
+    // private int spriteIndex;
+    // public Sprite[] idleSprites;
+    public Animator animator;
 
     private void Awake() {
         spriteRenderer = GetComponent<SpriteRenderer>();
         playerRigidBody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        healthPoints = 100f;
+        isGrounded = false;
+        facingRight = true;
     }
     // Start is called before the first frame update
-    void Start()
-    {
-        InvokeRepeating(nameof(AnimateSprite), 0.15f, 0.15f);
-    }
+    // void Start()
+    // {
+    //     InvokeRepeating(nameof(AnimateSprite), 0.15f, 0.15f);
+    // }
 
-    private void AnimateSprite() {
-        spriteIndex++;
-        if (spriteIndex >= idleSprites.Length) {
-            spriteIndex = 0;
-        }
-        spriteRenderer.sprite = idleSprites[spriteIndex];
-    }
+    // private void AnimateSprite() {
+    //     spriteIndex++;
+    //     if (spriteIndex >= idleSprites.Length) {
+    //         spriteIndex = 0;
+    //     }
+    //     spriteRenderer.sprite = idleSprites[spriteIndex];
+    // }
 
     // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
         if (Input.GetKey(KeyCode.A)) {
             playerRigidBody.velocity = new Vector2(-5, playerRigidBody.velocity.y);
-            transform.localScale = new Vector3(-2f, 2f, 1f);
+            if (facingRight) {
+                transform.localScale = new Vector3(-2f, 2f, 1f);
+                facingRight = false;
+            }
         }
         if (Input.GetKey(KeyCode.D)) {
             playerRigidBody.velocity = new Vector2(5, playerRigidBody.velocity.y);
-            transform.localScale = new Vector3(2f, 2f, 1f);
+            if (!facingRight) {
+                transform.localScale = new Vector3(2f, 2f, 1f);
+                facingRight = true;
+            }
+        }
+        if (Input.GetKey(KeyCode.W) && isGrounded) {
+            playerRigidBody.AddForce(Vector2.up * 20f, ForceMode2D.Impulse);
+            isGrounded = false;
+            animator.SetBool("isJumping", !isGrounded);
+        }
+        // if collision, update health points --> sprite change automatic, so just handle logic for death and respawn if occursunity    
+    }
 
-        }
-        if (Input.GetKey(KeyCode.W) && playerRigidBody.velocity.y == 0) {
-            playerRigidBody.AddForce(Vector2.up * 15f, ForceMode2D.Impulse);
-        }
-        
+    private void OnCollisionEnter2D(Collision2D collision) {
+        isGrounded = true;
     }
 }
